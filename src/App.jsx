f45d96cab449ef1be88df9b687a36f586358c058
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-02 16:54:23 -0300

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { FiSearch } from 'react-icons/fi'
+import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles/styles.css';
 
 const App = () => {
   return (
@@ -15,16 +16,16 @@ const App = () => {
         <button className="buttonSearch">
           <FiSearch size={25} color="#FFF" />
         </button>
-
-        <main className="main">
-          <h2>CEP: 79003222</h2>
-
-          <span>Rua </span>
-          <span>Complemento: </span>
-          <span>Sertão do Maruim</span>
-          <span>São José - SC</span>
-        </main>
       </div>
+      <main className="main">
+        <h2>CEP: 79003222</h2>
+
+        <span>Rua </span>
+        <span>Complemento: </span>
+        <span>Sertão do Maruim</span>
+        <span>São José - SC</span>
+      </main>
+
     </div>
   )
 }
