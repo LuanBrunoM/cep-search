4af9aa0289368dac0c93b33dab04e32f65503f92
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-02 15:44:30 -0300

    create container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { FiSearch } from 'react-icons/fi'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='container'>
+      <h1 className='title'>CEP Search</h1>
+
+      <div className='containerInput'>
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+
+        <main className="main">
+          <h2>CEP: 79003222</h2>
+
+          <span>Rua </span>
+          <span>Complemento: </span>
+          <span>Sertão do Maruim</span>
+          <span>São José - SC</span>
+        </main>
+      </div>
+    </div>
   )
 }
 
